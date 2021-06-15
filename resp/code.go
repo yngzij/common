5c35138f721814e763fd6f912d53a2b47368548c
yngzij <yngzij@gmail.com>
2021-06-16 02:28:09 +0800

    add 'error status'

--- resp/code.go
+++ resp/code.go
@@ -9,17 +9,19 @@ import (
 type Errno int
 
 const (
-	ok        = 200 // 成功
-	BindError = 201 // 参数解析错误
+	ok            = 200 // 成功
+	BindError     = 201 // 参数解析错误
+	InternelError = 202
 )
 
 var HTTPErrno = map[Errno]string{
-	BindError: "参数解析错误",
+	BindError:     "参数解析错误",
+	InternelError: "服务器内部错误",
 }
 
 func NewError(errno Errno, ctx *gin.Context) {
 	ctx.JSON(http.StatusBadRequest, gin.H{
-		"code":    BindError,
-		"message": HTTPErrno[BindError],
+		"code":    errno,
+		"message": HTTPErrno[errno],
 	})
 }
