d361bfba27ebdbd833747ee6eaef0256dc1cea8a
yngzij <yngzij@gmail.com>
2021-07-08 00:13:59 +0800

    feat: error code

--- resp/code.go
+++ resp/code.go
@@ -9,16 +9,20 @@ import (
 type Errno int
 
 const (
-	ok            = 200 // 成功
-	BindError     = 201 // 参数解析错误
-	InternalError = 202
-	IdNoneError   = 203
+	ok               = 200 // 成功
+	BindError        = 201 // 参数解析错误
+	InternalError    = 202
+	IdNoneError      = 203
+	CreateRepeated   = 204
+	CreateGroupError = 205
 )
 
 var HTTPErrno = map[Errno]string{
-	BindError:     "参数解析错误",
-	InternalError: "服务器内部错误",
-	IdNoneError:   "服务器内部错误",
+	BindError:        "参数解析错误",
+	InternalError:    "服务器内部错误",
+	IdNoneError:      "没有查找到用户",
+	CreateRepeated:   "重复创建",
+	CreateGroupError: "创建群组失败",
 }
 
 func NewError(errno Errno, ctx *gin.Context) {
